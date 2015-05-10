f64c6bd4c697262824afe77aa579da013b8ee0f7
Tom <absentisinaventulus@gmail.com>
2015-05-10 04:09:30 -0500

    Updated functions

--- latex_machine.py
+++ latex_machine.py
@@ -22,6 +22,7 @@ def main():
 	q = Queue.Queue()
 
 	# Open mentions, check for any new mentions
+	'''
 	with open('last_mention.txt', 'r+') as last_mentions:
 		# Read our last mention
 		last_mention_id = last_mentions.read()
@@ -43,11 +44,17 @@ def main():
 
 		else:
 			print 'Nobody needs me :('
+		'''
+
+	
+	latex_string = parse('@Latex_Machine >> \lambda Hi there! \omega >= 0')
+	print latex_string
 
 
 def execute(q, mention):
-	response = {'user':mention.user,'message':'thomasmoll.co'}
+	response = {'user':mention.user, 'tweet':mention, 'message':'thomasmoll.co'}
 
+	print mention.text
 	latex_string = parse(mention.text)
 	response['message'] = 'Hi there'
 
@@ -57,7 +64,17 @@ def execute(q, mention):
 
 def parse(string):
 	# Unescape html text
+
 	text = HTMLParser.HTMLParser().unescape(string)
+	
+	if '>>' in text:
+		latex = text.split('>> ')[1]
+	else:
+		# go to the website link, w/e
+		pass
+
+	return latex
+
 
 def make_latex(string):
 	''' Make the image, return the path for uploading '''
